2fc32836172d8e3413eb39e692dacbb28c43fc18
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-02-09 23:26:40 +0000

    Add files via upload

---
+++ Recursion/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+    public static int factorial(int n) {
+        if (n == 1) {
+            System.out.println("factorial (" + n + ") = 1");
+            return 1;
+        } else {
+            System.out.println("factorial (" + n + ") = " + n + " * factorial (" + (n - 1) + ")");
+            return n * factorial(n - 1);
+        }
+    }
+}
